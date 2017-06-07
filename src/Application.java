189fd261f2ceba44a19707bc0087b58174163587
George Paley <George@JDPaley.com>
2017-06-07 17:03:06 -0400

    Working version

--- src/Application.java
+++ src/Application.java
@@ -9,16 +9,25 @@ public class Application {
 		Connection con = null;
 		Statement stmt = null;
 		ResultSet rs = null;
-		String sql = "select * from customers";
+		//String sql = "select * from customer limit 9";
+		String sql = "select person.first_name, person.last_name, pet.name from person, pet limit 9";
 		try{
 			Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost/Customers?"
+            /* con = DriverManager.getConnection("jdbc:mysql://localhost/Customers?"
                                 + "user=root&password=password");
+                                */
+            con = DriverManager.getConnection("jdbc:mysql://localhost/pets?"
+                    + "user=root&password=password");
+            
 			stmt = con.createStatement();
 			rs = stmt.executeQuery(sql);
 			while(rs.next()){
-				System.out.print(rs.getString("FirstName") + "\t");
-				System.out.print(rs.getString("LastName"));
+/*				System.out.print(rs.getString("FirstName") + "\t");
+				System.out.print(rs.getString("LastName"));*/
+				
+				System.out.print(rs.getString("first_name") + " " + rs.getString("last_name") + "'s pet is:  \t");
+				System.out.print(rs.getString("name"));
+				
 				System.out.println();
 			}
 			}catch (SQLException e) {
